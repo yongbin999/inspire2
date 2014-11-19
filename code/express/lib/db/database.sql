631ee7d6a23e581b75373e6a46ee9795ed0eaa9b
Sam Fox <sdfox@umass.edu>
2014-11-18 20:08:02 -0500

    db is coming together

--- code/express/lib/db/database.sql
+++ code/express/lib/db/database.sql
@@ -1,11 +1,19 @@
 drop table if exists students;
-drop table if exists address;
-drop table if exists lives;
+drop table if exists coursecatalog;
+drop table if exists admins;
+drop table if exists prerequisites;
+drop table if exists offeredcourses;
+drop table if exists reportcards;
 
-create type level as enum ('freshman', 'sophomore', 'junior', 'senior');
+drop type if exists level; 
+drop type if exists grade;
+drop type if exists sem;
+drop type if exists offered;
+
+create type level as enum ('Freshman', 'Sophomore', 'Junior', 'Senior');
 create type grade as enum ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F');
 create type sem as enum ('Fall', 'Spring', 'Summer');
-create type offered as enum ('fall', 'spring', 'summer', 'fall/spring', 'fall/summer', 'spring/summer', 'fall/spring/summer');
+create type offered as enum ('Fall', 'Spring', 'Summer', 'Fall/Spring', 'Fall/Summer', 'Spring/Summer', 'Fall/Spring/Summer');
 
 create table admins (
 	adminid varchar(16),
@@ -27,93 +35,54 @@ create table students (
 create table coursecatalog (
 	coursenumber int,
 	coursename varchar(50),
+	description text,
 	credits int,
-	prerequisites int, /*Make this other courses?*/
 	semestersoffered offered,
 	primary key (coursenumber)
 );
 
-/*Might not need this...*/
+create table prerequisites (
+	coursenumber int,
+	prereq int
+);
+
 create table offeredcourses (
+	year int,
+	semester sem,
 	foreign key (coursenumber) references coursecatalog,
 	capacity int,
 	enrolled int,
-	description text,
-	instructor varchar(50),
-	semester sem,
-	year int,
-	primary key (coursenumber) /*Is this legal?*/
+	instructor varchar(50)
 );
 
 create table reportcards (
 	foreign key (id) references students,
-	foreign key (coursenumber) references offeredcourses,
+	foreign key (coursenumber) references coursecatalog,
 	coursegrade grade
 );
 
 insert into admins values ('timrichards', 'password', 'Tim', 'Richards');
 insert into admins values ('rodgrupen', 'password', 'Rod', 'Grupen');
 
-insert into students values ('samfox', 'password', 'Sam', 'Fox', 'senior', 4.0);
-insert into students values ('yongliang', 'password', 'Yong', 'Liang', 'junior', 4.0);
-insert into students values ('alexrevello', 'password', 'Alex', 'Revello', 'sophomore', 4.0);
-insert into students values ('joshbearor', 'password', 'Josh', 'Bearor', 'freshman', 4.0);
-insert into students values ('drewmarchetti', 'password', 'Drew', 'Marchetti', 'senior', 4.0);
-insert into students values ('khanhnguyen', 'password', 'Khanh', 'Nguyen', 'freshman', 4.0);
+insert into students values ('samfox', 'password', 'Sam', 'Fox', 'Senior', 4.0);
+insert into students values ('yongliang', 'password', 'Yong', 'Liang', 'Junior', 4.0);
+insert into students values ('alexrevello', 'password', 'Alex', 'Revello', 'Sophomore', 4.0);
+insert into students values ('joshbearor', 'password', 'Josh', 'Bearor', 'Freshman', 4.0);
+insert into students values ('drewmarchetti', 'password', 'Drew', 'Marchetti', 'Senior', 4.0);
+insert into students values ('khanhnguyen', 'password', 'Khanh', 'Nguyen', 'Freshman', 4.0);
 
-insert into coursecatalog values (105, 'Computer Literacy', 3, 0, 'fall/spring/summer');
-insert into coursecatalog values (119, 'Introduction to Programming', 3, 0, 'fall/spring');	
-insert into coursecatalog values (121, 'Introduction to Problem Solving With Computers', 4, 0, 'fall/spring');
+insert into coursecatalog values (105, 'Computer Literacy', 'You learn about shit', 3, 'Fall/Spring/Summer');
+insert into coursecatalog values (119, 'Introduction to Programming', 'You learn about shit', 3, 'Fall/Spring');	
+insert into coursecatalog values (121, 'Introduction to Problem Solving With Computers', 'You learn about shit', 4, 'Fall/Spring');
+insert into coursecatalog values (187, 'Programming with Data Structures', 'You learn about shit', 4, 'Fall/Spring');
 
-/*create table users (
-  uid SERIAL,
-  fname varchar(50),
-  lname varchar(50),
-  password varchar(25),
-  age int,
-  primary key (uid)
-  );*/
+insert into prerequisites values (187, 105);
+insert into prerequisites values (187, 119);
 
-/*create table address (
-	aid SERIAL,
-	street varchar(100),
-	city varchar(50),
-	state varchar(2),
-	zipcode varchar(9),
---	uid int,
-	primary key (aid)
-);
+insert into offeredcourses values (2014, 'Fall', 105, 200, 150, 'Verts');
+insert into offeredcourses values (2014, 'Fall', 119, 120, 91, 'Verts');
+insert into offeredcourses values (2014, 'Fall', 121, 165, 165, 'Anderson, Moll');
+insert into offeredcourses values (2014, 'Fall', 187, 60, 57, 'Barrington');
 
-create table lives (
-	uid int,
-	aid int,
-	foreign key (uid) references users,
-	foreign key (aid) references address,
-	unique(uid,aid)
-);*/
-
-
-
-/*insert into users values (1, 'John', 'Doe', 'xxxx', 27);
-insert into users values (2, 'Jane', 'Doe', 'yyyy', 28);
-insert into users values (3, 'Bill', 'Flood', 'aaaa', 29);
-insert into users values (4, 'Veb', 'Nordhagen', 'bbbb', 30);
-insert into users values (5, 'Hazel', 'Nutting', 'cccc', 4);
-insert into users values (6, 'Caleb', 'Manu', 'dddd', 7);
-insert into users values (7, 'Aiden', 'Hall', 'eeee', 19);*/
-
-/*insert into address values (1, '1 mallard drive', 'cambridge', 'MA', '34567');
-insert into address values (2, '21 jump street', 'new york', 'NY', '98765');
-insert into address values (3, '4 cherry lane', 'truffala', 'NJ', '58235');
-insert into address values (4, '16 strong road', 'chutney', 'VT', '38573');
-insert into address values (5, '99 livingston circle', 'amherst', 'MA', '99822');
-insert into address values (6, '1123 main street', 'worcester', 'MA', '22234');
-
-insert into lives values
-	(1, 6),
-	(2, 4),
-	(3, 6),
-	(4, 2),
-	(5, 5),
-	(6, 1),
-	(7, 3);*/
+insert into reportcards values ('samfox', 187, 'F');
+insert into reportcards values ('samfox', 121, 'B+');
--- code/express/lib/inspire-client.js
+++ code/express/lib/inspire-client.js
@@ -120,56 +120,7 @@ var single_user_handler = createResponseHandler(function (data) {
 
 //Added handler for returning all user data
 var all_users_handler = createResponseHandler(function (data) {
-  var entries = data.split("},");
-  console.log("\n\n");
-  //Chop off the [ at the beginning of the first one
-  entries[0] = entries[0].substring(1, (entries[0].length));
-  var outputString = ""; //String that will go to output
-  //Printing iteration
-  for(var i=0; i<entries.length; i++) {
-    //re-initialize outputString each time
-    outputString = "";
-    //Break elements into an array
-    var elements = entries[i].split(",");
-    //Append uid
-    var uid = elements[0].substring(7, elements[0].length);
-    outputString += "UID: " + uid + "\n";
-    //Append first name
-    var fname = elements[1].substring(9, elements[1].length-1);
-    outputString += "First Name: " + fname + "\n";
-    //Append last name
-    var lname = elements[2].substring(9, elements[2].length-1);
-    outputString += "Last Name: " + lname + "\n";
-    //Append password
-    var password = elements[3].substring(12, elements[3].length-1);
-    outputString += "Password: " + password + "\n";
-    //Append age
-    var age = elements[4].substring(6, elements[4].length);
-    outputString += "Age: " + age + "\n";
-    //Append city
-    var city = elements[5].substring(8, elements[5].length-1);
-    outputString += "City: " + city + "\n";
-    //Append AID
-    var aid = elements[6].substring(6, elements[6].length);
-    outputString += "AID: " + aid + "\n";
-    //Append street
-    var street = elements[7].substring(10, elements[7].length-1);
-    outputString += "Street: " + street + "\n";
-    //Append state
-    var state = elements[8].substring(9, elements[8].length-1);
-    outputString += "State: " + state + "\n";
-    //Append zipcode
-    //The last zipcode has 2 extra characters at the end...
-    if(i === entries.length-1) {
-      var zipcode = elements[9].substring(11, elements[9].length-3);
-    }
-    else {
-      var zipcode = elements[9].substring(11, elements[9].length-1);
-    }
-    outputString += "Zipcode: " + zipcode + "\n\n\n";
-    //Print it
-    console.log(outputString);
-  }
+  console.log(data);
 });
 
 console.log(' --> connecting to ' + options.host + ' on port ' + options.port);
