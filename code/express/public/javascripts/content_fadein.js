e9b4c2a11f05d6b7cfa6c5122e12e9688b7e13b5
yongbin999 <yongbinliang@umass.edu>
2014-11-23 13:22:34 -0500

    ajajavascript live table

--- code/express/public/javascripts/content_fadein.js
+++ code/express/public/javascripts/content_fadein.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 		    $("#content").fadeIn("slow");
+			$("#myclasses").fadeIn(3000);
+			$("#mycourses").fadeIn(3000);
+			$("#testing").fadeIn(3000);
+			$(".schedule").fadeIn(3000);
 		});
